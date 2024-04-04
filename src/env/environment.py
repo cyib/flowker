8b48948fe9f6e791efaeec5a1e769fa33c1c6164
Lucas Borges <lucas.campos.borges@gmail.com>
2024-04-03 23:38:08 -0300

    add env folder to src

---
+++ src/env/environment.py
@@ -0,0 +1,10 @@
+DB_HOST = 'localhost'
+DB_PORT = 3306
+DB_USER = 'dev'
+DB_PASSWORD = 'pass'
+DB_NAME = 'ioflow'
+
+FLOWKER_BASE_PATH = f'D:\\Projects\\flowker'
+FLOWKER_CLIENT_PATH = f'{FLOWKER_BASE_PATH}\\client'
+FLOWKER_SNAPSHOTS_PATH = f'{FLOWKER_CLIENT_PATH}\\snapshots'
+FLOWKER_SCRIPTS_PATH = f'{FLOWKER_CLIENT_PATH}\\scripts'
